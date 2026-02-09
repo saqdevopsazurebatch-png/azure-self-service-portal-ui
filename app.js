501fb571207f3c3ec3e15b1be6ad7acd949409be
saqdevopsazurebatch-png <saqdevopsazurebatch@gmail.com>
2026-02-09 23:14:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+async function callBackend() {
+    const response = await fetch("https://self-service-backend-api.azurewebsites.net/api/provision");
+    const text = await response.text();
+    alert(text);
+}
 
